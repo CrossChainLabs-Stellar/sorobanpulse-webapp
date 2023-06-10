93874a18e59d27a73254b34486ddecc7833a6527
backlitRO <cvlad1508@gmail.com>
2023-06-10 17:32:20 +0300

    added breakpoints

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,7 +14,7 @@ import ProjectsTable from "./ProjectsTable";
 export default function Dashboard() {
 
     return (
-        <Box sx={{ marginX: '24rem' }}>
+        <Box sx={{ marginX: { xl: '24rem', lg: '8rem' } }}>
             <Grid container spacing={3}>
 
                 <Grid item xs={12} md={3} lg={4}>
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -24,5 +24,13 @@ export default createTheme({
         }
 
     },
-
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1900,
+        },
+    },
 });
