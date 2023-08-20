a6b78783130d1a8ee682f08d715003ea8505343d
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2023-08-20 12:28:04 +0300

    Update config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API = process.env.REACT_APP_API || 'http://localhost:4000/';//'https://sorobanpulse.io/api/';
+//export const API = process.env.REACT_APP_API || 'http://localhost:4000/';
+export const API = process.env.REACT_APP_API || 'http://35.222.208.128/api/';//'https://sorobanpulse.io/api/';
